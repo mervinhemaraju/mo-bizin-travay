971664ede75d024ad8bf4d7747766d4c272dd788
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-03-10 15:37:05 +0400

    - Added missing requirements file - Added iam role actions for scheduler

--- iac/iam.tf
+++ iac/iam.tf
@@ -10,6 +10,27 @@ resource "aws_iam_policy" "lambda_actions" {
 
 # Scheduler IAM Role
 resource "aws_iam_role" "scheduler" {
-  name               = "mo-bizin-travay-scheduler-role"
-  assume_role_policy = file("${path.module}/policies/iam_scheduler_role_trust.json")
+  name                = "mo-bizin-travay-scheduler-role"
+  description         = "The scheduler role for the project mo-bizin-travay"
+  assume_role_policy  = file("${path.module}/policies/iam_scheduler_role_trust.json")
+  managed_policy_arns = [aws_iam_policy.scheduler.arn]
+
+  depends_on = [aws_iam_policy.scheduler]
+}
+
+# Scheduler IAM Policy
+resource "aws_iam_policy" "scheduler" {
+
+  name        = "mo-bizin-travay-scheduler-policy"
+  path        = "/"
+  description = "The scheduler actions for the project mo-bizin-travay"
+  policy = templatefile(
+    "${path.module}/policies/iam_scheduler_role_actions.json",
+    {
+      lambda_arn = module.openings_scraping.lambda_function_arn,
+      iam_arn    = module.openings_scraping.lambda_role_arn
+    }
+  )
+
+  depends_on = [module.openings_scraping]
 }
