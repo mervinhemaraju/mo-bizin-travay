a57a03278d6a81ac4098622c338b787c163188ad
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-03-14 10:23:21 +0400

    - Fixed database table creation

--- iac/database.tf
+++ iac/database.tf
@@ -1,54 +1,28 @@
-module "mo_bizin_travay_table" {
-
-  # Module info
-  source  = "terraform-aws-modules/dynamodb-table/aws"
-  version = "4.0.0"
-
-  # Basic DB Info
-  name     = var.db_table_name
-  hash_key = "id"
-
-  # Capacity configs
+resource "aws_dynamodb_table" "mo_bizin_travay" {
+  name           = var.db_table_name
   billing_mode   = "PROVISIONED"
   read_capacity  = 15
   write_capacity = 10
+  hash_key       = "id"
 
-  # Attributes
-  attributes = [
-    {
-      name = "id"
-      type = "S"
-    },
-    {
-      name = "posted_date"
-      type = "S"
-    },
-    {
-      name = "updated_at"
-      type = "S"
-    },
-    {
-      name = "recruiter"
-      type = "S"
-    },
-  ]
+  stream_enabled = false
 
-  # GSI
-  global_secondary_indexes = [
-    {
-      name            = "recruiter_index"
-      hash_key        = "recruiter"
-      projection_type = "ALL"
-    },
-    {
-      name            = "posted_date_index"
-      hash_key        = "posted_date"
-      projection_type = "ALL"
-    },
-    {
-      name            = "updated_at_index"
-      hash_key        = "updated_at"
-      projection_type = "ALL"
-    }
-  ]
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "recruiter"
+    type = "S"
+  }
+
+
+  global_secondary_index {
+    name            = "recruiter_index"
+    hash_key        = "recruiter"
+    write_capacity  = 10
+    read_capacity   = 10
+    projection_type = "ALL"
+  }
 }
--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,10 +1,10 @@
 
 output "dynamodb_mo_bizin_travay_table_arn" {
-  value = module.mo_bizin_travay_table.dynamodb_table_arn
+  value = aws_dynamodb_table.mo_bizin_travay.arn
 }
 
 output "dynamodb_mo_bizin_travay_table_id" {
-  value = module.mo_bizin_travay_table.dynamodb_table_id
+  value = aws_dynamodb_table.mo_bizin_travay.id
 }
 output "dynamodb_mo_bizin_travay_table_name" {
   value = var.db_table_name
