418d902be04a61d0d715d965a7e3f34cbcde4f7d
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-11 17:20:32 +0400

    - Disabled lambda and schedulers temporarily

--- iac/lambda.tf
+++ iac/lambda.tf
@@ -1,53 +1,53 @@
 
-# * Lambda Function module
-module "openings_scraping" {
+# # * Lambda Function module
+# module "openings_scraping" {
 
-  for_each = { for target in local.all_targets : target.source => target }
+#   for_each = { for target in local.all_targets : target.source => target }
 
-  # * source module info
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "~> 4"
+#   # * source module info
+#   source  = "terraform-aws-modules/lambda/aws"
+#   version = "~> 4"
 
-  # * Function basic info
-  function_name = "${local.lambda.prefix_name}-${lower(each.key)}"
-  description   = "The lambda function Mo Bizin Travay for the recruiter ${each.key}"
-  handler       = "main.main"
+#   # * Function basic info
+#   function_name = "${local.lambda.prefix_name}-${lower(each.key)}"
+#   description   = "The lambda function Mo Bizin Travay for the recruiter ${each.key}"
+#   handler       = "main.main"
 
-  # * Function advance info
-  memory_size                       = 1024
-  cloudwatch_logs_retention_in_days = 14
+#   # * Function advance info
+#   memory_size                       = 1024
+#   cloudwatch_logs_retention_in_days = 14
 
-  timeout                   = 600
-  create_async_event_config = false
-  maximum_retry_attempts    = 0
+#   timeout                   = 600
+#   create_async_event_config = false
+#   maximum_retry_attempts    = 0
 
-  # * Policies attachment
-  create_role = false
-  lambda_role = aws_iam_role.lambda.arn
+#   # * Policies attachment
+#   create_role = false
+#   lambda_role = aws_iam_role.lambda.arn
 
-  create_package = false
-  package_type   = "Image"
-  image_uri      = data.aws_ecr_image.mobizintravay.image_uri
+#   create_package = false
+#   package_type   = "Image"
+#   image_uri      = data.aws_ecr_image.mobizintravay.image_uri
 
-  environment_variables = {
-    DELAY                         = 15
-    SECRETS_CLOUD_IAC_TOKEN       = var.token_doppler_iac_cloud_main
-    SECRETS_DATABASE_ACCESS_TOKEN = var.token_doppler_database_secrets
-    DB_HOST                       = var.sm_mongo_host
-    SLACK_CHANNEL                 = var.slack_channel
-    SOURCE                        = each.key
-    SOURCE_URL                    = each.value.source_url
-    STARTUP_URL                   = each.value.startup_url
-    DOMAIN                        = each.value.domain
-  }
+#   environment_variables = {
+#     DELAY                         = 15
+#     SECRETS_CLOUD_IAC_TOKEN       = var.token_doppler_iac_cloud_main
+#     SECRETS_DATABASE_ACCESS_TOKEN = var.token_doppler_database_secrets
+#     DB_HOST                       = var.sm_mongo_host
+#     SLACK_CHANNEL                 = var.slack_channel
+#     SOURCE                        = each.key
+#     SOURCE_URL                    = each.value.source_url
+#     STARTUP_URL                   = each.value.startup_url
+#     DOMAIN                        = each.value.domain
+#   }
 
-  trusted_entities = [
-    {
-      type = "Service",
-      identifiers = [
-        "lambda.amazonaws.com"
-      ]
-    }
-  ]
-}
+#   trusted_entities = [
+#     {
+#       type = "Service",
+#       identifiers = [
+#         "lambda.amazonaws.com"
+#       ]
+#     }
+#   ]
+# }
 
--- iac/scheduler.tf
+++ iac/scheduler.tf
@@ -1,35 +1,35 @@
 
-resource "aws_scheduler_schedule_group" "lambda_trigger" {
-  name = local.schedule_group.name
-}
+# resource "aws_scheduler_schedule_group" "lambda_trigger" {
+#   name = local.schedule_group.name
+# }
 
-resource "aws_scheduler_schedule" "lambda_scheduler" {
-  for_each = { for target in local.all_targets : target.source => target }
+# resource "aws_scheduler_schedule" "lambda_scheduler" {
+#   for_each = { for target in local.all_targets : target.source => target }
 
-  name                = "schedule-${local.lambda.prefix_name}-${lower(each.key)}"
-  description         = "The schedule for ${local.lambda.prefix_name}-${lower(each.key)}"
-  group_name          = aws_scheduler_schedule_group.lambda_trigger.name
-  schedule_expression = "rate(4 days)"
-  state               = "ENABLED"
+#   name                = "schedule-${local.lambda.prefix_name}-${lower(each.key)}"
+#   description         = "The schedule for ${local.lambda.prefix_name}-${lower(each.key)}"
+#   group_name          = aws_scheduler_schedule_group.lambda_trigger.name
+#   schedule_expression = "rate(4 days)"
+#   state               = "ENABLED"
 
-  flexible_time_window {
-    mode = "OFF"
-  }
+#   flexible_time_window {
+#     mode = "OFF"
+#   }
 
-  target {
-    arn      = "arn:aws:lambda:${var.region}:${data.aws_caller_identity.current.account_id}:function:${local.lambda.prefix_name}-${lower(each.key)}"
-    role_arn = aws_iam_role.scheduler.arn
+#   target {
+#     arn      = "arn:aws:lambda:${var.region}:${data.aws_caller_identity.current.account_id}:function:${local.lambda.prefix_name}-${lower(each.key)}"
+#     role_arn = aws_iam_role.scheduler.arn
 
-    retry_policy {
-      maximum_retry_attempts = 0
-    }
+#     retry_policy {
+#       maximum_retry_attempts = 0
+#     }
 
-    input = jsonencode(
-      {
-        filters = each.value.filters
-      }
-    )
-  }
+#     input = jsonencode(
+#       {
+#         filters = each.value.filters
+#       }
+#     )
+#   }
 
-  depends_on = [module.openings_scraping, aws_scheduler_schedule_group.lambda_trigger]
-}
+#   depends_on = [module.openings_scraping, aws_scheduler_schedule_group.lambda_trigger]
+# }
